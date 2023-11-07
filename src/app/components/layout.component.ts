2974eb2f7ab21971f4ea2bd2389568db93737fe7
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-11-07 17:09:58 +0700

    update description

--- src/app/components/layout.component.ts
+++ src/app/components/layout.component.ts
@@ -7,148 +7,80 @@ import { LogoComponent } from './logo.component';
   standalone: true,
   imports: [CommonModule, LogoComponent],
   template: `
-    <div class="drawer">
-      <input id="app-drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex w-screen flex-col">
-        <div
-          class="navbar min-h-[3.75em] w-full bg-[rgba(26,26,26,1)] px-5 lg:px-6"
-        >
-          <a href="/" class="cursor-pointer">
-            <app-logo />
-          </a>
-          <label
-            htmlFor="app-drawer"
-            class="btn-ghost btn-square btn ml-auto lg:hidden"
+    <div class="flex w-screen flex-col">
+      <div
+        class="navbar min-h-[3.75em] w-full bg-[rgba(26,26,26,1)] px-5 lg:px-6"
+      >
+        <a href="/" class="cursor-pointer">
+          <app-logo />
+        </a>
+        <div class="ml-auto flex gap-2 ">
+          <a
+            target="_blank"
+            href="https://twitter.com/runyasak"
+            class="btn-ghost !min-h-8 btn !h-8 font-normal"
           >
             <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
             >
               <path
-                d="M3 18V16H21V18H3ZM3 13V11H21V13H3ZM3 8V6H21V8H3Z"
                 fill="white"
+                d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23Z"
               />
             </svg>
-          </label>
-          <div class="ml-auto hidden flex-none gap-2 lg:flex">
-            <!-- <Link
-          target="_blank"
-          href="https://www.threads.net/@aideatelab"
-          class="btn-ghost !min-h-8 btn !h-8 font-normal"
-        >
-          <img class="h-5" src="/thread_logo.png" alt="" />
-        </Link> -->
-            <!-- <Link
-          target="_blank"
-          href="https://twitter.com/aideatelab"
-          class="btn-ghost !min-h-8 btn !h-8 font-normal"
-        >
-          <img class="h-5" src="/twitter_logo.png" alt="" />
-        </Link> -->
-            <!-- <Link
-          href="/#faq"
-          class="btn-ghost !min-h-8 btn !h-8 font-normal"
-        >
-          FAQ
-        </Link> -->
-            <!-- <Link
-          href="https://app.aideatelab.com"
-          class="btn-ghost !min-h-8 btn !h-8 font-normal"
-        >
-          Sign in
-        </Link> -->
-            <!-- <Link
-          href="https://app.aideatelab.com"
-          class="btn-primary !min-h-8 btn !h-8 font-normal"
-        >
-          Get Started
-        </Link> -->
-          </div>
-        </div>
-
-        <div class="min-h-[calc(100vh-17.5em)] lg:min-h-[calc(100vh-15.5em)]">
-          <ng-content></ng-content>
-        </div>
-
-        <div
-          class="container mx-auto flex flex-col gap-6 px-5 py-10 lg:gap-4 lg:px-[7.5em] lg:py-[3.75em]"
-        >
-          <div
-            class="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between"
+          </a>
+          <a
+            target="_blank"
+            href="https://github.com/runyasak"
+            class="btn-ghost !min-h-8 btn !h-8 font-normal"
           >
-            <!-- <Link href="/" class="cursor-pointer">
-          <AppLogo logoClassName="w-[1.125em]" />
-        </Link> -->
-
-            <div class="flex gap-8">
-              <a class="link-hover link font-bold">Product</a>
-              <!-- <Link href="/#faq" class="link-hover link font-bold">
-            FAQ
-          </Link> -->
-              <a class="link-hover link font-bold">Contact us</a>
-            </div>
-          </div>
-
-          <div class="text-sm text-secondary">
-            © 2023 AI-Deate |
-            <!-- <Link href="/terms-of-use" class="link-hover link">
-          Terms of Use
-        </Link>
-        |
-        <Link href="/privacy-policy" class="link-hover link">
-          Privacy Policy
-        </Link>
-        |
-        <Link href="/content-policy" class="link-hover link">
-          Content Policy
-        </Link> -->
-          </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="white"
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+              />
+            </svg>
+          </a>
         </div>
       </div>
-      <div class="drawer-side">
-        <label htmlFor="app-drawer" class="drawer-overlay"></label>
-        <ul class="menu h-full w-80 bg-[rgba(26,26,26,1)] px-2 py-3">
-          <div class="flex items-center px-2">
-            <!-- <AppLogo /> -->
-            <label
-              htmlFor="app-drawer"
-              class="btn-ghost btn-square btn ml-auto justify-end"
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z"
-                  fill="white"
-                />
-              </svg>
-            </label>
-          </div>
 
-          <li>
-            <a class="px-2 text-base">Home</a>
-          </li>
-          <li>
-            <!-- <DrawerLink href="/#faq">FAQ</DrawerLink> -->
-          </li>
-          <li>
-            <!-- <Link
-          href="https://app.aideatelab.com"
-          class="px-2 text-base"
+      <div class="min-h-[calc(100vh-17.5em)] lg:min-h-[calc(100vh-15.5em)]">
+        <ng-content></ng-content>
+      </div>
+
+      <div
+        class="container mx-auto flex flex-col gap-6 px-5 py-10 lg:gap-4 lg:px-[7.5em] lg:py-[3.75em]"
+      >
+        <div
+          class="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between"
         >
-          Sign in
-        </Link> -->
-          </li>
+          <a href="/" class="cursor-pointer">
+            <app-logo class="w-[1.125em]" />
+          </a>
 
-          <button class="btn-primary btn mx-2 my-3">Get Started</button>
-        </ul>
+          <div class="flex gap-8">
+            <a class="link-hover link font-bold">Product</a>
+            <a href="/" class="link-hover link font-bold"> FAQ </a>
+            <a class="link-hover link font-bold">Contact us</a>
+          </div>
+        </div>
+
+        <div class="text-sm text-secondary">
+          © 2023 Lorem |
+          <a href="/" class="link-hover link"> Terms of Use </a>
+          |
+          <a href="/" class="link-hover link"> Privacy Policy </a>
+          |
+          <a href="/" class="link-hover link"> Content Policy </a>
+        </div>
       </div>
     </div>
   `,
